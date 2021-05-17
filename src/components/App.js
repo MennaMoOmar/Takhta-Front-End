d50ca84d8c074de77d053b136933e93b5af55e3e
menna <menna.mohamed.omar@gmail.com>
2021-05-17 20:22:45 +0200

    add Sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
-// import "../sass/main.scss"
+import "../Sass/main.scss"
 
 const App = () => {
   return (

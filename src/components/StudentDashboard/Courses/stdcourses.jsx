7f205199327474a34a3a2fce909c6331809438ce
menna <menna.mohamed.omar@gmail.com>
2021-05-22 20:53:31 +0200

    edit routing

--- src/components/StudentDashboard/Courses/stdcourses.jsx
+++ src/components/StudentDashboard/Courses/stdcourses.jsx
@@ -1,52 +1,27 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 const StdCourses = () => {
+  /* history */
+  const history = useHistory();
+
+  const handleChooseCourse = (id) => {
+    history.push(`/stddashboardlectures/${id}`);
+  };
+
   return (
     <React.Fragment>
       <section className="stdcourses">
         <div className="row  justify-content-md-center">
           <div className="col-md-4 col-sm-6 stdcourses__coursecard">
-            <div className="stdcourses__coursename">
-              Science
+            <div
+              className="stdcourses__coursename"
+              onClick={() => handleChooseCourse(1)}
+            >
+              React
               <img
                 className="navBar__image stdcourses__img"
                 src="/images/science.png"
-                alt="logo"
-              ></img>
-            </div>
-            <div className="stdcourses__courseteacher">Teacher: Malak</div>
-          </div>
-
-          <div className="col-md-4 col-sm-6 stdcourses__coursecard">
-            <div className="stdcourses__coursename">
-              Science
-              <img
-                className="navBar__image stdcourses__img"
-                src="/images/science.png"
-                alt="logo"
-              ></img>
-            </div>
-            <div className="stdcourses__courseteacher">Teacher: Menna</div>
-          </div>
-
-          <div className="col-md-4 col-sm-6 stdcourses__coursecard">
-            <div className="stdcourses__coursename">
-              Math
-              <img
-                className="navBar__image stdcourses__img"
-                src="/images/math.png"
-                alt="logo"
-              ></img>
-            </div>
-            <div className="stdcourses__courseteacher">Teacher: Malak</div>
-          </div>
-
-          <div className="col-md-4 col-sm-6 stdcourses__coursecard">
-            <div className="stdcourses__coursename">
-              English
-              <img
-                className="navBar__image stdcourses__img"
-                src="/images/english.png"
                 alt="logo"
               ></img>
             </div>

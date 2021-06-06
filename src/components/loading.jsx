08a6506f33dd5a5595816f9b64e63d20ffe154ac
menna <menna.mohamed.omar@gmail.com>
2021-06-06 13:28:37 +0200

    create loading component

---
+++ src/components/loading.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Loading = () => (
+  <>
+    <div className="loading">
+      <div class="loadingio-spinner-interwind-tn9sijnxk4">
+        <div class="ldio-81dyc4p55yx">
+          <div>
+            <div>
+              <div>
+                <div></div>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <img className="loading__img" src="/images/loading.gif" alt="" /> */}
+    </div>
+  </>
+);
+
+export default Loading;
